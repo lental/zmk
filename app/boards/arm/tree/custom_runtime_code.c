c999e2548bc1c318ea507dc34cdfb12d7165932c
lental <me@michaellen.com>
2022-12-27 22:15:31 -0800

    got central  nano working.   ble keys stink

--- app/boards/arm/tree/custom_runtime_code.c
+++ app/boards/arm/tree/custom_runtime_code.c
@@ -19,11 +19,51 @@ static const struct gpio_dt_spec led_two = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), g
 static const struct gpio_dt_spec led_three = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led2), gpios,
 						     {0});
 
+/**
+ * When the BT is connected, turn on an LED
+ */
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed (err 0x%02x)\n", err);
+		int err = gpio_pin_set_dt(&led_two, 0);
+		if (err) {
+			printk("LED Set failed (err 0x%02x)\n", err);
+		}
+	} else {
+		int err = gpio_pin_set_dt(&led_two, 1);
+		if (err) {
+			printk("LED Set failed (err 0x%02x)\n", err);
+		}
+		printk("Connected\n");
+	}
+}
+
+/**
+ * When the BT is disconnected, turn o ff an LED
+ */
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+		int err = gpio_pin_set_dt(&led_two, 0);
+		if (err) {
+			printk("LED Set failed (err 0x%02x)\n", err);
+		}
+	printk("Disconnected (reason 0x%02x)\n", reason);
+}
+
+// Configure the connection callbacks
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+
 static int init(const struct device *port) {
 		printk("HEllooo tree startho\n");
 	int ret = gpio_pin_configure_dt(&led_one, GPIO_OUTPUT_ACTIVE);
 	ret = gpio_pin_configure_dt(&led_two, GPIO_OUTPUT_INACTIVE);
-	ret = gpio_pin_configure_dt(&led_three, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&led_three, GPIO_OUTPUT_INACTIVE);
+
 	return 0;	
 }
 
