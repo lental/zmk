f0b3f28caf35479d9bcc04f264491ab030ff0401
lental <me@michaellen.com>
2022-12-28 11:35:06 -0800

    got tree keymaps to work the way  it's configured

--- app/boards/arm/tree/custom_runtime_code.c
+++ app/boards/arm/tree/custom_runtime_code.c
@@ -12,11 +12,11 @@
 #include <sys/sys_io.h>
 #include <devicetree.h>
 
-static const struct gpio_dt_spec led_one = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
+static const struct gpio_dt_spec led_r = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
 						     {0});
-static const struct gpio_dt_spec led_two = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpios,
+static const struct gpio_dt_spec led_g = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpios,
 						     {0});
-static const struct gpio_dt_spec led_three = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led2), gpios,
+static const struct gpio_dt_spec led_b = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led2), gpios,
 						     {0});
 
 /**
@@ -26,12 +26,16 @@ static void connected(struct bt_conn *conn, uint8_t err)
 {
 	if (err) {
 		printk("Connection failed (err 0x%02x)\n", err);
-		int err = gpio_pin_set_dt(&led_two, 0);
+		int err = gpio_pin_set_dt(&led_r, 1);
+		err = gpio_pin_set_dt(&led_g, 0);
+		err = gpio_pin_set_dt(&led_b, 0);
 		if (err) {
 			printk("LED Set failed (err 0x%02x)\n", err);
 		}
 	} else {
-		int err = gpio_pin_set_dt(&led_two, 1);
+		int err = gpio_pin_set_dt(&led_r, 0);
+		err = gpio_pin_set_dt(&led_g, 0);
+		err = gpio_pin_set_dt(&led_b, 0);
 		if (err) {
 			printk("LED Set failed (err 0x%02x)\n", err);
 		}
@@ -44,7 +48,9 @@ static void connected(struct bt_conn *conn, uint8_t err)
  */
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
-		int err = gpio_pin_set_dt(&led_two, 0);
+		int err = gpio_pin_set_dt(&led_r, 1);
+		err = gpio_pin_set_dt(&led_g, 0);
+		err = gpio_pin_set_dt(&led_b, 0);
 		if (err) {
 			printk("LED Set failed (err 0x%02x)\n", err);
 		}
@@ -60,9 +66,9 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 
 static int init(const struct device *port) {
 		printk("HEllooo tree startho\n");
-	int ret = gpio_pin_configure_dt(&led_one, GPIO_OUTPUT_ACTIVE);
-	ret = gpio_pin_configure_dt(&led_two, GPIO_OUTPUT_INACTIVE);
-	ret = gpio_pin_configure_dt(&led_three, GPIO_OUTPUT_INACTIVE);
+	int ret = gpio_pin_configure_dt(&led_r, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&led_g, GPIO_OUTPUT_INACTIVE);
+	ret = gpio_pin_configure_dt(&led_b, GPIO_OUTPUT_INACTIVE);
 
 	return 0;	
 }
