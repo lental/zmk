d59a4bc0239652737156339a66eb2660a5c0992b
lental <me@michaellen.com>
2024-07-27 09:25:39 -0700

    Update  for Tree   v2

--- app/boards/arm/tree/custom_runtime_code.c
+++ app/boards/arm/tree/custom_runtime_code.c
@@ -18,42 +18,52 @@ static const struct gpio_dt_spec led_g = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpi
 						     {0});
 static const struct gpio_dt_spec led_b = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led2), gpios,
 						     {0});
+static const struct gpio_dt_spec led_conn = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led3), gpios,
+						     {0});
+
+
+static void setLedsDisconnected() {
+	int err = gpio_pin_set_dt(&led_conn, 1);
+	err = gpio_pin_set_dt(&led_r, 0);
+	err = gpio_pin_set_dt(&led_g, 0);
+	err = gpio_pin_set_dt(&led_b, 0);
+
+	if (err) {
+		printk("LED Set failed (err 0x%02x)\n", err);
+	}
+}
+
+static void setLedsConnected() {
+	int err = gpio_pin_set_dt(&led_conn, 0);
+	err = gpio_pin_set_dt(&led_r, 0);
+	err = gpio_pin_set_dt(&led_g, 0);
+	err = gpio_pin_set_dt(&led_b, 0);
+	
+	if (err) {
+		printk("LED Set failed (err 0x%02x)\n", err);
+	}
+}
 
 /**
- * When the BT is connected, turn on an LED
+ * When the BT is connected, turn off an LED
  */
 static void connected(struct bt_conn *conn, uint8_t err)
 {
 	if (err) {
 		printk("Connection failed (err 0x%02x)\n", err);
-		int err = gpio_pin_set_dt(&led_r, 1);
-		err = gpio_pin_set_dt(&led_g, 0);
-		err = gpio_pin_set_dt(&led_b, 0);
-		if (err) {
-			printk("LED Set failed (err 0x%02x)\n", err);
-		}
+		setLedsDisconnected();
 	} else {
-		int err = gpio_pin_set_dt(&led_r, 0);
-		err = gpio_pin_set_dt(&led_g, 0);
-		err = gpio_pin_set_dt(&led_b, 0);
-		if (err) {
-			printk("LED Set failed (err 0x%02x)\n", err);
-		}
+		setLedsConnected();
 		printk("Connected\n");
 	}
 }
 
 /**
- * When the BT is disconnected, turn o ff an LED
+ * When the BT is disconnected, turn on an LED
  */
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
-		int err = gpio_pin_set_dt(&led_r, 1);
-		err = gpio_pin_set_dt(&led_g, 0);
-		err = gpio_pin_set_dt(&led_b, 0);
-		if (err) {
-			printk("LED Set failed (err 0x%02x)\n", err);
-		}
+	setLedsDisconnected();
 	printk("Disconnected (reason 0x%02x)\n", reason);
 }
 
@@ -65,8 +75,9 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 
 
 static int init(const struct device *port) {
-		printk("HEllooo tree startho\n");
-	int ret = gpio_pin_configure_dt(&led_r, GPIO_OUTPUT_ACTIVE);
+		printk("HEllooo tree start\n");
+	int ret = gpio_pin_configure_dt(&led_conn, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&led_r, GPIO_OUTPUT_INACTIVE);
 	ret = gpio_pin_configure_dt(&led_g, GPIO_OUTPUT_INACTIVE);
 	ret = gpio_pin_configure_dt(&led_b, GPIO_OUTPUT_INACTIVE);
 
